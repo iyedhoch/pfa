2bb317f1a55e03849b83083796f93d03d330adb5
Skander Jaouadi <iskanderjaouadi99@gmail.com>
2025-04-27 23:00:29 +0200

    main update

--- pfa-front-end/src/main.jsx
+++ pfa-front-end/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import WelcomePage from './Pages/welcomePage'
+import WelcomePage from './Pages/WelcomePage'
 import NotFound from './Pages/NotFound'
-<<<<<<< Updated upstream
-=======
 import Reserve from './Pages/Reserve'
 import Login from './Pages/Login'
 import Register from './Pages/register'
 import DashboardPage from './Pages/Dashboard'
->>>>>>> Stashed changes
 
 
 const router= createBrowserRouter([
@@ -22,9 +19,6 @@ const router= createBrowserRouter([
   {
     path:"*",
     element:<NotFound/>
-<<<<<<< Updated upstream
-  }
-=======
   },
   {
     path:"/reserve",
@@ -42,7 +36,6 @@ const router= createBrowserRouter([
     path:"/Dashboard",
     element:<DashboardPage/>
   },
->>>>>>> Stashed changes
 
 ])
 
